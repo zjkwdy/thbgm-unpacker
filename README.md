a954eb53eacddb1357df37afa26fe8fb41b19819
zjkwdy <3377911508@qq.com>
2022-01-31 12:19:14 +0800

    支持直接提取wav

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # THBGM
 
-一个用来生成`BgmForAll.ini`的脚本。懒得测试那么多了，目前就测试了
+一个用来提取东方正作bgm的脚本，顺便可以生成`BgmForAll.ini`。懒得测试那么多了，目前就测试了
 
 th07-妖妖梦、th12-星莲船。
 
 首先使用`thtk`解压出`thbgm.fmt`，放在跟脚本同一个目录。
+然后把正作文件夹内`thbgm.dat`也放到跟脚本同一个目录。
 
 然后就可以运行这个脚本了```python thbgm.py```
 
-然后把生成的`BgmForAll.ini`和正作`thbgm.dat`放在和`BgmForAll.exe`一个文件夹就能打开听歌了
+不出意外的话会生成一大堆wav和一个`BgmForAll.ini`。
+
+注释都在`thbgm.py`，自己看吧。没什么技术含量。
--- thbgm.py
+++ thbgm.py
@@ -1,4 +1,3 @@
-from aifc import Error
 from os.path import exists
 from io import BufferedReader
 from configparser import RawConfigParser
@@ -32,7 +31,7 @@ class thfmt:
 
     def __init__(self, fmt: str = 'thbgm.fmt') -> None:
         if not exists(fmt):
-            raise FileNotFoundError(f'File {fmt} Not Found,exiting..')
+            raise FileNotFoundError(f'找不到{fmt}，退出...')
         self.fileName = fmt
         f = open(fmt, 'rb')
         bgmList = []
@@ -47,8 +46,7 @@ class thfmt:
                 fileName = bgm[:16].replace(b'\x00', b'').decode('utf-8')
                 # 文档来自网络，都要小端序排列
                 startTime = int.from_bytes(bgm[16:20], 'little')  # bgm开始偏移量
-                duration = int.from_bytes(
-                    bgm[20:24], 'little')  # bgm时长（大——概——吧——）
+                duration = int.from_bytes(bgm[20:24], 'little')  # bgm时长
                 loopStart = int.from_bytes(bgm[24:28], 'little')  # 循环节开始
                 loopDuration = int.from_bytes(bgm[28:32], 'little')  # 循环长度
                 bgm = {
@@ -64,9 +62,71 @@ class thfmt:
     def close(self) -> None:
         self.fmt.close()
 
+
+class bgmdat:
+
+    dat: BufferedReader
+
+    def __init__(self, fileName='thbgm.dat') -> None:
+        if not exists(fileName):
+            raise FileNotFoundError(f'找不到{fileName},退出...')
+        self.dat = open(fileName, 'rb')
+
+    def close(self) -> None:
+        self.dat.close()
+
+    def seek(self, postion: int) -> int:
+        return self.dat.seek(postion,0)
+
+    def read(self, size) -> bytes:
+        return self.dat.read(size)
+
+
+class riff:
+    # riff header
+    RIFF_HEADER = b'\x52\x49\x46\x46'  # RIFF
+    riff_size = b'\x00\x00\x00\x00'  # 4字节的数据长度
+    WAVE = b'\x57\x41\x56\x45'
+
+    # fmt chunk start
+    FMT = b'\x66\x6d\x74\x20'  # fmt
+    PCM_FMT = b'\x10\x00\x00\x00'
+    COMPRESS = b'\x01\x00'
+    CHANNELS = b'\x02\x00'
+    SAMPLE = b'\x44\xac\x00\x00'  # 44100采样率
+    BYTE_RATE = b'\x10\xb1\x02\x00'
+    BLOCK_ALIGN = b'\x04\x00'
+    SAMPLE_DEPTH = b'\x10\x00'
+
+    # data chunk start
+    DATA_HEADER = b'\x64\x61\x74\x61'  # 'data'
+    DATA_SIZE = b'\x00\x00\x00\x00'
+    DATA = b'\x00\x00\x00\x00'
+
+    BYTESDATA = [
+        RIFF_HEADER, riff_size, WAVE,  # RIFF CHUNK
+        FMT, PCM_FMT, COMPRESS, CHANNELS, SAMPLE, BYTE_RATE, BLOCK_ALIGN, SAMPLE_DEPTH,  # FMT CHUNK
+        DATA_HEADER, DATA_SIZE, DATA  # DATA CHUNK
+    ]
+
+    def __init__(self, data: bytes) -> None:
+        duration = len(data)
+        self.DATA_SIZE = duration.to_bytes(4, 'little')
+        self.riff_size = (0x24+duration).to_bytes(4, 'little')
+        self.DATA = data
+        self.BYTESDATA[1] = self.riff_size
+        self.BYTESDATA[12] = self.DATA_SIZE
+        self.BYTESDATA[13] = self.DATA
+
+    def getBytes(self) -> bytes:
+        return b''.join(self.BYTESDATA)
+
+    def save(self, fileName: str) -> None:
+        with open(fileName, 'wb') as fp:
+            fp.write(self.getBytes())
+
+
 # 继承重写配置文件类，使其支持大写。。
-
-
 class myconf(RawConfigParser):
     def __init__(self, defaults=None):
         RawConfigParser.__init__(self, defaults=None)
@@ -89,6 +149,7 @@ config.set('THBGM', 'BIT', '16')
 
 # 打开bgm.fmt,初始化
 fmt = thfmt('thbgm.fmt')
+dat = bgmdat('thbgm.dat')
 with open('BgmForAll.ini', 'w+') as ini:
     # 写入死参数
     config.write(ini)
@@ -98,10 +159,16 @@ with open('BgmForAll.ini', 'w+') as ini:
         lS = hex(bgm.loopSart)
         x1 = hex(bgm.startTime+bgm.loopSart)
         x2 = hex(bgm.loopDuration-bgm.loopSart)
+        print(nE)
+        dat.seek(bgm.startTime)
+        byte=dat.read(bgm.loopDuration)
+        wav=riff(byte)
+        wav.save(nE)
         bgm = 'BGM = %s,%s,%s,%s,%s\n' % (nE, sT, lS, x1, x2)
 
         bgm = bgm.upper().replace('0X', '0x')
-        print(bgm)
+        # print(bgm)
+
         ini.write(bgm)
 
 # 经典无用代码
